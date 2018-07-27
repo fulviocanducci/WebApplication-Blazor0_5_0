4a64aadca03afc32645b4771c138a82dcce2c609
Fúlvio <fulviocanducci@hotmail.com>
2018-07-26 23:40:19 -0300

    Add project files.

---
+++ WebApplication7.App/Services/ZipCode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.App.Services
+{    
+    public class ZipCode
+    {
+        public ZipCode() { }       
+
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+        public string Unidade { get; set; }
+        public string Ibge { get; set; }
+        public string Gia { get; set; }
+
+        public static implicit operator bool(ZipCode z)
+        {
+            return z.Cep != null;
+        }
+    }
+
+}
