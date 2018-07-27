09724ab9048c3d6c51d8b998f01f5dbd807fc62c
Fúlvio <fulviocanducci@hotmail.com>
2018-07-27 13:11:01 -0300

    Components

--- Client.Library/JsExample.cs
+++ Client.Library/JsExample.cs
@@ -12,5 +12,19 @@ namespace Client.Library
                     .Current
                     .InvokeAsync<object>("functions.myAlert", message);
         }
+
+        public static Task MySweetAlertSuccess(string title, string message)
+        {
+            return JSRuntime
+                    .Current
+                    .InvokeAsync<object>("functions.mySweetAlertSuccess", title, message);
+        }
+
+        public static Task MyConsole(string message)
+        {
+            return JSRuntime
+                    .Current
+                    .InvokeAsync<object>("functions.myConsole", message);
+        }
     }
 }
--- Client.Library/content/JsExample.js
+++ Client.Library/content/JsExample.js
@@ -1,5 +1,11 @@
 ﻿window.functions = {
     myAlert: function (message) {
         alert(message);
+    },
+    mySweetAlertSuccess: function (title, message) {
+        swal(title, message,'success');
+    },
+    myConsole: function (message) {
+        console.log(message);
     }
 }
